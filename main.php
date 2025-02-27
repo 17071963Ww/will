c6acc202c9df4f8d1b979e720030ee5925704d13
willian pinoti <willian.pinotti@unidavi.edu.br>
2025-02-27 19:58:25 -0300

    teste

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_REQUEST['nome'];
+    $telefone = $_REQUEST['numero'];
+    $email = $_REQUEST['email'];
+    $mensagem = $_REQUEST['mensagem'];
+  
+    echo "<h1>Dados Enviados:</h1>";
+    echo "<p>Nome: $nome</p>";
+    echo "<p>Telefone: $telefone</p>";
+    echo "<p>Email: $email</p>";
+    echo "<p>Mensagem: $mensagem</p>";
+}
+?>
